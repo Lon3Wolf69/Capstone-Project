fbf5ba4e0af0c11b177bad465c57d899c228a2be
Nnahhaa <167724340+Nnahhaa@users.noreply.github.com>
2026-03-28 17:57:59 -0400

    arghhhh
    my b if it messes up things

--- Frontend/src/Components/About/Why/Why.jsx
+++ Frontend/src/Components/About/Why/Why.jsx
@@ -19,19 +19,34 @@ const why = () => {
         <div className="cards-section">
             {/* First card  */}
             <div className="card">
-                <div className="card-icon">🏗️</div>
+                <div className="card-icon">  
+                <img 
+                    src="https://www.citypng.com/public/uploads/preview/hd-magnifying-glass-question-mark-orange-icon-png-701751694962499adguvkaswd.png"
+                    alt="Idea icon"
+                />
+                </div>
                 <h2 className="card-header">The Challenge</h2>
                 <p>States first define priorities across housing, infrastructure, economic, environmental, and health needs. But as execution begins, gaps appear: misaligned funding, limited capacity, and operational bottlenecks.</p>
             </div>
             {/* Second card */}
             <div className="card">
-                <div className="card-icon">🤝</div>
+                <div className="card-icon">
+                <img 
+                    src="https://freesvg.org/img/1490609861.png"
+                    alt="Idea icon"
+                />
+                </div>
                 <h2 className="card-header">Our Solution</h2>
                 <p>DisasterReady brings the system together - aligning agencies, partners, and resources to move recovery forward with clarity and momentum.</p>
             </div>
             {/* Third card */}
             <div className="card">
-                <div className="card-icon">✅</div>
+                <div className="card-icon">
+                <img 
+                    src="https://www.clipartmax.com/png/middle/312-3127906_orange-checkmark-clipart-check-mark-clip-art-orange-tick-mark-png.png"
+                    alt="Idea icon"
+                />
+                </div>
                 <h2 className="card-header">The Result</h2>
                 <p>Because coordinated recovery is successful recovery! When the right people, tools, and plans align - communities rebuild faster and stronger.</p>
             </div>
