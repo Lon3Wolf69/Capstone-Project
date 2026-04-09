89cd55e201aa1726afa55f75114adc594dff1ede
ZacHSouza <zackarys428@gmail.com>
2026-04-09 11:08:59 -0400

    Fixed issue with formatting on the intro of Meet the Team

--- Frontend/src/Components/Pages/AboutPage/MeetTeam/TeamIntro.jsx
+++ Frontend/src/Components/Pages/AboutPage/MeetTeam/TeamIntro.jsx
@@ -2,12 +2,12 @@ import "./TeamIntro.css";
 
 export default function TeamIntro() {
   return (
-    <div className="container">
-        <h2 className="title">
+    <div className="intro-container">
+        <h2 className="intro-title">
           Introduction
         </h2>
 
-        <p className="para">
+        <p className="intro-para">
           DisasterReady brings together leaders with 
           deep experience in federal disaster response, 
           recovery coordination, and operational execution. 
@@ -16,7 +16,7 @@ export default function TeamIntro() {
           where recovery succeeds and where it breaks down.
         </p>
 
-        <p className="para">
+        <p className="intro-para">
           This experience informs how we approach 
           coordination, ensuring recovery systems 
           work together to deliver real outcomes.
